e2b109c68d38a6c4d82473de776b012bb16cb274
liukedian <91513633+liukedian@users.noreply.github.com>
2023-12-05 14:55:24 +0800

    搭建关注的社区listview和adapter

--- app/src/main/java/com/example/android_demo/ui/community/CommunityFragment.java
+++ app/src/main/java/com/example/android_demo/ui/community/CommunityFragment.java
@@ -4,13 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.android_demo.R;
 import com.example.android_demo.databinding.FragmentCommunityBinding;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class CommunityFragment extends Fragment {
 
@@ -22,10 +29,8 @@ public class CommunityFragment extends Fragment {
                 new ViewModelProvider(this).get(CommunityViewModel.class);
 
         binding = FragmentCommunityBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
-        final TextView textView = binding.textCommunity;
-        communityViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        View root = binding.getRoot();
         return root;
     }
 
--- app/src/main/java/com/example/android_demo/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/android_demo/ui/home/HomeFragment.java
@@ -4,19 +4,71 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.android_demo.MainViewModel;
+import com.example.android_demo.R;
 import com.example.android_demo.databinding.FragmentHomeBinding;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    List<HashMap<String,Object>> list = new ArrayList<>();
+
+    public List<HashMap<String,Object>> putData(){
+        HashMap<String,Object> map1 = new HashMap<>();
+        map1.put("name", "代码随想录");
+        map1.put("id", "coder");
+        map1.put("age", "2岁");
+        map1.put("tel", "18322228898");
+        map1.put("pic", R.drawable.sui);
+        HashMap<String,Object> map2 = new HashMap<>();
+        map2.put("name", "李四");
+        map2.put("id", "jyw8886");
+        map2.put("age", "27岁");
+        map2.put("tel", "13922278898");
+        map2.put("pic",  R.drawable.sui);
+        HashMap<String,Object> map3 = new HashMap<>();
+        map3.put("name", "王五");
+        map3.put("id", "bmw8899");
+        map3.put("age", "27岁");
+        map3.put("tel", "13319780706");
+        map3.put("pic",  R.drawable.sui);
+        HashMap<String,Object> map4 = new HashMap<>();
+        map4.put("name", "听雨");
+        map4.put("id", "wh445306");
+        map4.put("age", "28岁");
+        map4.put("tel", "17322228898");
+        map4.put("pic",  R.drawable.sui);
+        HashMap<String,Object> map5 = new HashMap<>();
+        map5.put("name", "若兰");
+        map5.put("id", "jyw8886");
+        map5.put("age", "27岁");
+        map5.put("tel", "13922278898");
+        map5.put("pic",  R.drawable.sui);
+        HashMap<String,Object> map6 = new HashMap<>();
+        map6.put("name", "海子");
+        map6.put("id", "bmw8899");
+        map6.put("age", "27岁");
+        map6.put("tel", "13319780706");
+        map6.put("pic",  R.drawable.sui);
+        list.add(map1);
+        list.add(map2);
+        list.add(map3);
+        list.add(map4);
+        list.add(map5);
+        list.add(map6);
+        return list;
+    }
     MainViewModel mainViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -27,8 +79,14 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        ListView listview = binding.lvTest;
+
+        putData();
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(getActivity(),list, R.layout.community_item_layout,
+                new String[]{"name","id","age","pic","tel"},
+                new int[]{R.id.txtCommunityName,R.id.txtCommunityID,R.id.txtUserAge,R.id.imgHead,R.id.txtUserTel});
+        listview.setAdapter(simpleAdapter);
 
         //尝试获取共享数据viewmodel
         mainViewModel=new ViewModelProvider(getActivity()).get(MainViewModel.class);
