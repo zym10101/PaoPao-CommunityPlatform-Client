d6dab6a0efa84b9bf78aa5a3a7c51e82adda2631
SummCoder <2404174187@qq.com>
2023-12-09 13:19:18 +0800

    社区板块

--- app/src/main/java/com/example/android_demo/community/InFragment.java
+++ app/src/main/java/com/example/android_demo/community/InFragment.java
@@ -1,14 +1,38 @@
 package com.example.android_demo.community;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.android_demo.Constants.constant;
+import com.example.android_demo.R;
+import com.example.android_demo.bean.CommunityBean;
 import com.example.android_demo.databinding.FragmentInBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * @author SummCoder
@@ -17,6 +41,10 @@ import com.example.android_demo.databinding.FragmentInBinding;
 public class InFragment extends Fragment {
     private FragmentInBinding binding;
 
+    List<CommunityBean> communityBeanList = new ArrayList<>();
+
+    MyAdapter myAdapter;
+    private RecyclerView recyclerview_in;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,6 +52,11 @@ public class InFragment extends Fragment {
         binding = FragmentInBinding.inflate(inflater, container, false);
 
         View root = binding.getRoot();
+        recyclerview_in = root.findViewById(R.id.recyclerview_in);
+        myAdapter = new MyAdapter();
+        recyclerview_in.setAdapter(myAdapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerview_in.setLayoutManager(layoutManager);
         return root;
     }
 
@@ -32,4 +65,95 @@ public class InFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        load();
+    }
+
+    public void load() {
+        new Thread(() -> {
+            try {
+                OkHttpClient client = new OkHttpClient()
+                        .newBuilder()
+                        .connectTimeout(60000, TimeUnit.MILLISECONDS)
+                        .readTimeout(60000, TimeUnit.MILLISECONDS)
+                        .build();
+                ;
+                String url = "http://" + constant.IP_ADDRESS + "/uc/getCommunityIdList";
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+                Response response = client.newCall(request).execute();
+                if (response.isSuccessful()) {
+                    String responseData = response.body().string();
+                    JSONArray jsonArray = new JSONArray(responseData);
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        // 获取社区id？
+                        long communityId = jsonArray.getLong(i);
+                    }
+                }
+            } catch (Exception e) {
+                Log.e(TAG, Log.getStackTraceString(e));
+            }
+        }).start();
+    }
+
+
+
+    class MyAdapter extends RecyclerView.Adapter<InFragment.MyViewHolder> {
+
+        @NonNull
+        @Override
+        public InFragment.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = View.inflate(getActivity(), R.layout.community_item, null);
+            return new MyViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull InFragment.MyViewHolder holder, int position) {
+            CommunityBean communityBean = communityBeanList.get(position);
+            holder.iv_cover.setImageResource(communityBean.cover);
+            holder.tv_community_name.setText(communityBean.name);
+            holder.tv_community_follow.setText(communityBean.follow);
+            // 为每个cb_community_follow添加OnCheckedChangeListener
+            holder.cb_community_follow.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    // 在这里定义选中状态改变时的操作
+                    String desc = String.format("%s", isChecked ? "已关注" : "关注");
+                    if (isChecked) {
+                        holder.cb_community_follow.setText(desc);
+                    } else {
+                        holder.cb_community_follow.setText(desc);
+                    }
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return communityBeanList.size();
+        }
+    }
+
+
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+        ImageView iv_cover;
+        TextView tv_community_name;
+
+        TextView tv_community_follow;
+
+        CheckBox cb_community_follow;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            iv_cover = itemView.findViewById(R.id.iv_cover);
+            tv_community_name = itemView.findViewById(R.id.tv_community_name);
+            tv_community_follow = itemView.findViewById(R.id.tv_community_follow);
+            cb_community_follow = itemView.findViewById(R.id.cb_community_follow);
+        }
+    }
+
 }
--- app/src/main/java/com/example/android_demo/community/RecommendFragment.java
+++ app/src/main/java/com/example/android_demo/community/RecommendFragment.java
@@ -4,11 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.android_demo.R;
+import com.example.android_demo.bean.CommunityBean;
 import com.example.android_demo.databinding.FragmentRecommendBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author SummCoder
@@ -17,6 +27,29 @@ import com.example.android_demo.databinding.FragmentRecommendBinding;
 public class RecommendFragment extends Fragment {
     private FragmentRecommendBinding binding;
 
+    List<CommunityBean> communityBeanList = new ArrayList<>();
+
+    MyAdapter myAdapter;
+
+    private static int[] coverArray = {R.drawable.cover0, R.drawable.cover1, R.drawable.cover2};
+    private static String[] nameArray = {
+            "薅羊毛小分队",
+            "今日热点",
+            "华为鸿蒙",
+            "手机摄影",
+            "沙雕乐园",
+            "开箱评测"
+    };
+    private static String[] followArray = {
+            "33.7万关注",
+            "4.2万关注",
+            "5.8万关注",
+            "11.3万关注",
+            "8.7万关注",
+            "7.2万关注"
+    };
+    private RecyclerView recyclerview_recommend;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,6 +57,15 @@ public class RecommendFragment extends Fragment {
         binding = FragmentRecommendBinding.inflate(inflater, container, false);
 
         View root = binding.getRoot();
+        recyclerview_recommend = root.findViewById(R.id.recyclerview_recommend);
+        for(int i = 0; i < 12; i++){
+            CommunityBean communityBean = new CommunityBean(coverArray[i%3], nameArray[i%6], followArray[i%6]);
+            communityBeanList.add(communityBean);
+        }
+        myAdapter = new MyAdapter();
+        recyclerview_recommend.setAdapter(myAdapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerview_recommend.setLayoutManager(layoutManager);
         return root;
     }
 
@@ -31,5 +73,46 @@ public class RecommendFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    class MyAdapter extends RecyclerView.Adapter<RecommendFragment.MyViewHolder>{
+
+        @NonNull
+        @Override
+        public RecommendFragment.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = View.inflate(getActivity(), R.layout.community_item, null);
+            return new MyViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecommendFragment.MyViewHolder holder, int position) {
+            CommunityBean communityBean = communityBeanList.get(position);
+            holder.iv_cover.setImageResource(communityBean.cover);
+            holder.tv_community_name.setText(communityBean.name);
+            holder.tv_community_follow.setText(communityBean.follow);
+            holder.cb_community_follow.setText("关注");
+        }
+
+        @Override
+        public int getItemCount() {
+            return communityBeanList.size();
+        }
+    }
+
+
+    static class MyViewHolder extends RecyclerView.ViewHolder{
+        ImageView iv_cover;
+        TextView tv_community_name;
+
+        TextView tv_community_follow;
+
+        CheckBox cb_community_follow;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            iv_cover = itemView.findViewById(R.id.iv_cover);
+            tv_community_name = itemView.findViewById(R.id.tv_community_name);
+            tv_community_follow = itemView.findViewById(R.id.tv_community_follow);
+            cb_community_follow = itemView.findViewById(R.id.cb_community_follow);
+        }
     }
 }
